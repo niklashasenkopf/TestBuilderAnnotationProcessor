fbdee16c65167e30cbc6e4a806dd6891d92dc0bf
Niklas Hasenkopf <n.hasenkopf@stud.unibas.ch>
2026-06-28 14:32:21 +0200

    Non-existing default files handled, informational warning

--- src/main/java/com/niklas/ObjectFormatter.java
+++ src/main/java/com/niklas/ObjectFormatter.java
@@ -6,6 +6,16 @@ public class ObjectFormatter {
 
     // TODO: Handle different types like lists, enums, nested objects
     public static Object formatValue(TypeName typeName, Object value) {
+        if (value == null) {
+            if (typeName.equals(TypeName.DOUBLE)) return 0.0;
+            if (typeName.equals(TypeName.INT))    return 0;
+            if (typeName.equals(TypeName.LONG))   return 0L;
+            if (typeName.equals(TypeName.FLOAT))  return 0.0f;
+            if (typeName.equals(TypeName.BOOLEAN)) return false;
+            if (typeName.isPrimitive()) return 0; // catch-all for short/byte/char
+            return null; // reference types: null is a legal initializer
+        }
+
         if (typeName.equals(TypeName.get(String.class))) {
             return "\"" + value + "\"";
         }
--- src/main/java/com/niklas/YamlReader.java
+++ src/main/java/com/niklas/YamlReader.java
@@ -2,11 +2,10 @@ package com.niklas;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.tools.StandardLocation;
 
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
@@ -15,22 +14,10 @@ import tools.jackson.dataformat.yaml.YAMLFactory;
 public class YamlReader {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(new YAMLFactory());
-    private ProcessingEnvironment processingEnvironment;
-    private String entityName;
 
-    public YamlReader(
-            ProcessingEnvironment processingEnvironment,
-            String entityName
-    ) {
-        this.processingEnvironment = processingEnvironment;
-        this.entityName = entityName;
-    }
+    public Map<String, Object> getDefaults(Path yaml) {
 
-    public Map<String, Object> getDefaults(Path yaml) throws IOException {
-
-        try (InputStream in = processingEnvironment.getFiler()
-                .getResource(StandardLocation.CLASS_PATH, "", "TestBuilders/" + entityName + ".yaml")
-                                                   .openInputStream()) {
+        try (InputStream in = Files.newInputStream(yaml)) {
             return OBJECT_MAPPER.readValue(in, new TypeReference<>() {});
         } catch (IOException e) {
             System.err.printf("Error reading yaml file: %s%n", e.getMessage());
